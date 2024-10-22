a6c3d1c696797134ab902a2d7649ff4fcf67d799
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-10-22 17:10:24 -0300

    feat: add Rut validation

---
+++ src/Helpers/RutValidations.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.src.Helpers
+{
+    public class RutValidations
+    {
+        public static bool IsValidRut(string rut)
+        {
+            if (string.IsNullOrWhiteSpace(rut))
+                return false;
+
+            rut = rut.Replace(".", "").Replace("-", "");
+
+            if (rut.Length < 9 || rut.Length > 10)
+                return false;
+
+            string dv = rut.Substring(rut.Length - 1, 1);
+            string rutBody = rut.Substring(0, rut.Length - 1);
+
+            if (!int.TryParse(rutBody, out int rutNumber))
+                return false;
+
+            return dv.ToUpper() == CalculateRutCheckDigit(rutNumber);
+        }
+
+        public static string CalculateRutCheckDigit(int rut)
+        {
+            int sum = 0;
+            int multiplier = 2;
+
+            while (rut > 0)
+            {
+                int digit = rut % 10;
+                sum += digit * multiplier;
+                multiplier = (multiplier == 7) ? 2 : multiplier + 1;
+                rut /= 10;
+            }
+
+            int remainder = 11 - (sum % 11);
+
+            if (remainder == 11) return "0";
+            if (remainder == 10) return "K";
+
+            return remainder.ToString();
+        }
+    }
+}
