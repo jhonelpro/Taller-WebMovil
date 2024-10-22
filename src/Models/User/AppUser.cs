2d29724e02953bb6861e87b1f1cec405a2c32c31
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-10-22 16:47:22 -0300

    chore: delete data notations in Models

--- src/Models/User/AppUser.cs
+++ src/Models/User/AppUser.cs
@@ -5,20 +5,14 @@ namespace api.src.Models.User
 {
     public class AppUser : IdentityUser
     {
-        [Required]
         public string? Rut { get; set; } = string.Empty;
 
-        [Required]
-        [StringLength(255, MinimumLength = 8, ErrorMessage = "Name must be between 8 and 255 characters.")]
         public string? Name { get; set; } = string.Empty;
 
-        [Required]
         public DateTime DateOfBirth { get; set; }
 
-        [Required]
-        [RegularExpression(@"Femenino|Masculino|Prefiero no decirlo|Otro", ErrorMessage = "Gender must be one of the specified values.")]
         public string? Gender { get; set; } = string.Empty;
-        [Required]
+
         public int IsActive { get; set; }
     }
 }
