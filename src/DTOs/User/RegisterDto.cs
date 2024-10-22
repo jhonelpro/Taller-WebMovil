612c39ba21b82d9f5224744b73d861f4284d505a
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-10-22 16:44:45 -0300

    feat: add validations in DTOs

--- src/DTOs/User/RegisterDto.cs
+++ src/DTOs/User/RegisterDto.cs
@@ -23,7 +23,8 @@ namespace api.src.DTOs.User
         public string? Email { get; set; } = string.Empty;
 
         [Required]
-        [MinLength(6)]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 20 characters long.")]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Password must be alphanumeric.")]
         public string? Password { get; set; } = string.Empty;
 
         [Required]
