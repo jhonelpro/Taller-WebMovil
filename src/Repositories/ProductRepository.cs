b0eed587564a58d665bb02e9cd879740418a78c0
jhonelpro <jhon.vallecilla@alumnos.ucn.cl>
2024-10-29 13:36:20 -0300

    fix: the methods for adding products are fixed

--- src/Repositories/ProductRepository.cs
+++ src/Repositories/ProductRepository.cs
@@ -51,6 +51,8 @@ namespace api.src.Repositories
                 ImageUrl = uploadResult.SecureUrl.AbsoluteUri
             };
 
+            newProduct.ProductType = await _context.ProductTypes.FirstOrDefaultAsync(p => p.Id == product.ProductTypeId) ?? throw new Exception("ProductType not found");
+
             await _context.Products.AddAsync(newProduct);
             await _context.SaveChangesAsync();
 
@@ -135,6 +137,7 @@ namespace api.src.Repositories
             existingProduct.Price = product.Price;
             existingProduct.Stock = product.Stock;
             existingProduct.ImageUrl = uploadResult.SecureUrl.AbsoluteUri;
+            existingProduct.ProductType = await _context.ProductTypes.FirstOrDefaultAsync(p => p.Id == product.ProductTypeId) ?? throw new Exception("ProductType not found");
             
             await _context.SaveChangesAsync();
 
