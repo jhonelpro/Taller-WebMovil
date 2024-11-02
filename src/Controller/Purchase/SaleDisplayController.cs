eecaa66528465ef88bdfe1bd089df0b8b1023fba
jhonelpro <jhon.vallecilla@alumnos.ucn.cl>
2024-11-02 18:55:17 -0300

    docs: document purchase controllers

--- src/Controller/Purchase/SaleDisplayController.cs
+++ src/Controller/Purchase/SaleDisplayController.cs
@@ -4,18 +4,43 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace api.src.Controller.Purchase
 {
+    /// <summary>
+    /// Controlador para mostrar las compras realizadas por todos los usuarios del sistema.
+    /// </summary>
+    /// <remarks>
+    /// Solo los usuarios con el rol de Admin pueden acceder a los endpoints de este controlador.
+    /// </remarks>
     [Route("api/[controller]")]
     [ApiController]
     [Authorize(Roles = "Admin")]
     public class SaleDisplayController : ControllerBase
     {
+        /// <summary>
+        /// Atributo de tipo ISaleItem que permite la inyección de dependencias.
+        /// </summary>
         private readonly ISaleItem _saleItem;
 
+        /// <summary>
+        /// Constructor de la clase SaleDisplayController que recibe un objeto de tipo ISaleItem.
+        /// </summary>
+        /// <param name="saleItem">Parámetro de tipo ISaleItem que sirve para inicializar el atributo _saleItem.</param>
         public SaleDisplayController(ISaleItem saleItem)
         {
+            // Inicializa del atributo _saleItem.
             _saleItem = saleItem;
         }
         
+        /// <summary>
+        /// Endpoint para mostrar todas las compras realizadas por los usuarios del sistema.
+        /// </summary>
+        /// <returns>
+        /// Retorna un objeto JSON con la información de las compras realizadas por los usuarios.
+        /// <list type="Bullet">
+        /// <item>200 Ok con la información de las compras realizadas.</item>
+        /// <item>404 Not Found si no se encuentran compras realizadas.</item>
+        /// <item>500 Internal Server Error si ocurre un error al intentar obtener la información de las compras realizadas.</item>
+        /// </list>
+        /// </returns>
         [HttpGet("SaleDisplay")]
         public async Task<IActionResult> SaleDisplay()
         {
