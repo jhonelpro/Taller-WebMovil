49d36777f8102be9c529f19b4a55478014e7a36b
jhonelpro <jhon.vallecilla@alumnos.ucn.cl>
2024-11-02 20:33:50 -0300

    docs: document user controllers

--- src/Controller/User/AuthController.cs
+++ src/Controller/User/AuthController.cs
@@ -11,23 +11,64 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace api.src.Controller
 {
+    /// <summary>
+    /// Controlador de autenticación de usuarios.
+    /// </summary>
+    /// <remarks>
+    /// Este controlador se encarga de manejar las peticiones de autenticación de usuarios.
+    /// las acciones que se pueden realizar son: registro, login y logout.
+    /// </remarks>
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
+        /// <summary>
+        /// Atributo de tipo UserManager<AppUser> que se encarga de manejar las operaciones de los usuarios.
+        /// </summary>
         private readonly UserManager<AppUser> _userManager;
+
+        /// <summary>
+        /// Atributo de tipo ITokenService que se encarga de manejar la creación de tokens.
+        /// </summary>
         private readonly ITokenService _tokenService;
+
+        /// <summary>
+        /// Atributo de tipo SignInManager<AppUser> que se encarga de manejar las operaciones de inicio de sesión.
+        /// </summary>
         private readonly SignInManager<AppUser> _signInManager;
+
+        /// <summary>
+        /// Atributo de tipo IShoppingCart que se encarga de manejar las operaciones de carrito de compras.
+        /// </summary>
         private readonly IShoppingCart _shoppingCart;
 
+        /// <summary>
+        /// Constructor de la clase AuthController que recibe un objeto de tipo UserManager<AppUser>, ITokenService, SignInManager<AppUser>, IShoppingCart.
+        /// </summary>
+        /// <param name="userManager">Parámetro de tipo UserManager<AppUser> que sirve para inicializar el atributo _userManager</param>
+        /// <param name="tokenService">Parámetro de tipo ITokenService que sirve para inicializar el atributo _tokenService</param>
+        /// <param name="signInManager">Parámetro de tipo SignInManager que sirve para inicializar el atributo _signInManager</param>
+        /// <param name="shoppingCart">Parámetro de tipo IShoppingCart que sirve para inicializar el atributo _shoppingCart</param>
         public AuthController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager, IShoppingCart shoppingCart)
         {
+            // Inicializa el atributo _userManager con el valor del parámetro userManager
             _userManager = userManager;
+            // Inicializa el atributo _tokenService con el valor del parámetro tokenService
             _tokenService = tokenService;
+            // Inicializa el atributo _signInManager con el valor del parámetro signInManager
             _signInManager = signInManager;
+            // Inicializa el atributo _shoppingCart con el valor del parámetro shoppingCart
             _shoppingCart = shoppingCart;
         }
 
+        /// <summary>
+        /// Endpoint para registrar un nuevo usuario.
+        /// </summary>
+        /// <param name="registerDto">Parámetro que representa un usuario el cual se registrara en el sistema.</param>
+        /// <returns>
+        /// Retorna un objeto de tipo IActionResult con el resultado de la operación.
+        /// Retorna un objeto de tipo NewUserDto con los datos del usuario y el token de autenticación.
+        /// </returns>
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
@@ -59,15 +100,18 @@ namespace api.src.Controller
                     Gender = registerDto.Gender,
                     IsActive = 1
                 };
-
+                
                 var createUser = await _userManager.CreateAsync(user, registerDto.Password);
 
+                // Si el usuario se crea correctamente
                 if (createUser.Succeeded)
                 {
+                    // Asignar rol de usuario
                     var roleResult = await _userManager.AddToRoleAsync(user, "User");
 
                     if (roleResult.Succeeded)
                     {
+                        // Crear carrito de compras para el usuario
                         await _shoppingCart.CreateShoppingCart(user.Id);
 
                         return Ok(new NewUserDto
@@ -94,6 +138,14 @@ namespace api.src.Controller
             }
         }
 
+        /// <summary>
+        /// Endpoint para iniciar sesión en el sistema.
+        /// </summary>
+        /// <param name="loginDto">Parámetro que representa un usuario que quiere iniciar sesión en el sistema.</param>
+        /// <returns>
+        /// Retorna un objeto de tipo IActionResult con el resultado de la operación.
+        /// Retorna un objeto de tipo NewUserDto con los datos del usuario y el token de autenticación.
+        /// </returns>
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
         {
@@ -101,17 +153,21 @@ namespace api.src.Controller
 
                 if(!ModelState.IsValid) return BadRequest(ModelState);
 
+                // Buscar usuario por nombre de usuario
                 var user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.UserName);
                 if(user == null) return Unauthorized("Invalid username or password.");
 
-
+                // Verificar si la contraseña es correcta
                 var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
                 if(!result.Succeeded) return Unauthorized("Invalid username or password.");
 
+                // Verificar si el usuario esta activo
                 if(user.IsActive == 0) return Unauthorized("User is not active.");
 
+                // Iniciar sesión
                 await _signInManager.SignInAsync(user, isPersistent: true);
 
+                // Crear token
                 var token = _tokenService.CreateToken(user);
 
                 if (string.IsNullOrEmpty(token)) return Unauthorized("Invalid token.");
@@ -130,6 +186,13 @@ namespace api.src.Controller
             }
         }
 
+        /// <summary>
+        /// Endpoint para cerrar sesión en el sistema.
+        /// </summary>
+        /// <returns>
+        /// Retorna un objeto de tipo IActionResult con el resultado de la operación.
+        /// Retorna un mensaje que indica si se cerro sesión correctamente.
+        /// </returns>
         [HttpPost("logout")]
         [Authorize(Roles = "User,Admin")]
         public async Task<IActionResult> Logout()
--- src/Controller/User/UserManagementController.cs
+++ src/Controller/User/UserManagementController.cs
@@ -8,18 +8,40 @@ using Microsoft.EntityFrameworkCore;
 
 namespace api.src.Controller
 {
+    /// <summary>
+    /// Controlador para la gestión de usuarios.
+    /// </summary>
+    /// <remarks>
+    /// En este controlador se encuentran los métodos necesarios para la gestión de usuarios.
+    /// Las operaciones que se pueden realizar son: obtener usuarios y cambiar el estado de un usuario.
+    /// </remarks>
     [Route("api/[controller]")]
     [ApiController]
     [Authorize(Roles = "Admin")]
     public class UserManagementController : ControllerBase
     {
+        /// <summary>
+        /// Atributo para la gestión de usuarios.
+        /// </summary>
         private readonly UserManager<AppUser> _userManager;
 
+        /// <summary>
+        /// Constructor de la clase UserManagementController que recibe un objeto de tipo UserManager<AppUser>.
+        /// </summary>
+        /// <param name="userManager">Parámetro que sirve para la inicializar el atributo _userManager</param>
         public UserManagementController(UserManager<AppUser> userManager)
         {
+            // Inicializa del atributo _userManager
             _userManager = userManager;
         }
 
+        /// <summary>
+        /// Endpoint para obtener los usuarios.
+        /// </summary>
+        /// <param name="query">Parámetro que permite realizar una solicitud que muestre los usuarios con un nombre en especifico</param>
+        /// <returns>
+        /// Retorna una lista con todos los usuarios de la base de datos que cumplan con los criterios de la query
+        /// </returns>
         [HttpPost("GetUsers")]
         public async Task<IActionResult> GetUsers([FromQuery] QueryObjectUsers query)
         {
@@ -29,10 +51,13 @@ namespace api.src.Controller
             {
                 var user = _userManager.Users.AsQueryable();
 
+                // Si el query no es nulo, se filtra por el nombre del usuario
                 if (!string.IsNullOrEmpty(query.Name)) user = user.Where(p => p.Name!.Contains(query.Name));
-
+                
+                // Se obtiene la lista de usuarios de forma paginada
                 var usersList = await user.Skip((query.Page - 1) * query.PageSize).Take(query.PageSize).ToListAsync();
 
+                // Se mapea la lista de usuarios a una lista de UserDto
                 var usersListDto = usersList.Select(u => UserMapper.MapUserToUserDto(u)).ToList();
                 
                 return Ok(usersListDto);                
@@ -43,6 +68,11 @@ namespace api.src.Controller
             }
         }
 
+        /// <summary>
+        /// Endpoint para cambiar el estado de un usuario.
+        /// </summary>
+        /// <param name="email">Parámetro que representa el email del usuario al cual se quiere deshabilitar o habilitar</param>
+        /// <returns></returns>
         [HttpPost("ChangeStateUser/{email}")]
         public async Task<IActionResult> ChangeStateUser(string email)
         {
@@ -59,14 +89,17 @@ namespace api.src.Controller
                     return BadRequest("User not found.");
                 }
 
+                // Se cambia el estado del usuario dependiendo de su estado actual
                 if (user.IsActive == 1){
                     user.IsActive = 0;
                 } else {
                     user.IsActive = 1;
                 }
 
+                // Se actualiza el usuario
                 await _userManager.UpdateAsync(user);
 
+                // Se mapea el usuario a un UserDto
                 return Ok(UserMapper.MapUserToUserDto(user));
             }
             catch (Exception ex)
