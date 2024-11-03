d63c2cdca34751acabf104a86a99c615fe1247ed
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-11-03 03:21:57 -0300

    docs: document shoppingCart dtos

--- src/DTOs/ShoppingCart/CartDto.cs
+++ src/DTOs/ShoppingCart/CartDto.cs
@@ -1,15 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using api.src.DTOs.Product;
 
 namespace api.src.DTOs.ShoppingCart
 {
+    /// <summary>
+    /// Clase CartDto que representa un carrito de compras, con los datos a mostrar al usuario.
+    /// </summary>
     public class CartDto
     {
+        /// <summary>
+        /// Atributo de tipo double que representa el precio total del carrito de compras.
+        /// </summary>
         public double Cart_TotalPrice { get; set; }
 
+        /// <summary>
+        /// Atributo de tipo List<ShoppingCartDto> que contiene los artículos en el carrito de compras.
+        /// </summary>
         public List<ShoppingCartDto> CartItems { get; set; } = new List<ShoppingCartDto>();
     }
 }
