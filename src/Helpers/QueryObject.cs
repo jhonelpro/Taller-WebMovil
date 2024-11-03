bcdd56afaeac87362dd981b82787f316e23f6116
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-11-03 13:55:32 -0300

    docs: document helpers.

--- src/Helpers/QueryObject.cs
+++ src/Helpers/QueryObject.cs
@@ -1,13 +1,43 @@
-
 namespace api.src.Helpers
 {
+    /// <summary>
+    /// Clase QueryObject que representa la solicitud que permite el filtro y paginado de los productos.
+    /// Contiene los parámetros necesarios para realizar filtros, ordenar y paginar la lista de productos.
+    /// </summary>
     public class QueryObject
     {
-        public string? textFilter { get; set; }= string.Empty;
+        /// <summary>
+        /// Atributo de tipo string que representa el texto por el cual el usuario desea filtrar los productos.
+        /// Permite buscar productos que coincidan con este texto.
+        /// </summary>
+        public string? textFilter { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Atributo de tipo string que representa el tipo de producto por el cual se desea filtrar.
+        /// Este filtro permite seleccionar productos que pertenecen a una categoría específica.
+        /// </summary>
         public string? productType { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Atributo de tipo string que representa el criterio para ordenar los productos por precio.
+        /// </summary>
         public string? sortByPrice { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Atributo de tipo bool que indica si se desea ordenar de forma descendente.
+        /// Al ser verdadero, se ordenarán los productos de mayor a menor; de lo contrario, de menor a mayor.
+        /// </summary>
         public bool IsDescending { get; set; } = false;
+
+        /// <summary>
+        /// Atributo de tipo int que representa el número de página que se desea ver.
+        /// </summary>
         public int pageNumber { get; set; } = 1;
+
+        /// <summary>
+        /// Atributo de tipo int que representa el tamaño de la página.
+        /// Define el número de elementos que se verán en cada página de resultados.
+        /// </summary>
         public int pageSize { get; set; } = 10;
     }
 }
--- src/Helpers/RutValidations.cs
+++ src/Helpers/RutValidations.cs
@@ -1,31 +1,50 @@
-
 namespace api.src.Helpers
 {
+    /// <summary>
+    /// Clase RutValidations que contiene métodos para la validación y cálculo del dígito verificador de un RUT chileno.
+    /// </summary>
     public class RutValidations
     {
+        /// <summary>
+        /// Valida si un RUT chileno es correcto verificando el formato y el dígito verificador.
+        /// </summary>
+        /// <param name="rut">Parametro de tipo string qu representa un RUT chileno, con puntos y guión.</param>
+        /// <returns>Devuelve true si el RUT es válido; de lo contrario, false.</returns>
         public static bool IsValidRut(string rut)
         {
+            // Verifica que el rut no este vacio.
             if (string.IsNullOrWhiteSpace(rut))
                 return false;
-
+            
+            // Verifica que el rut tenga el guion, y que el digito verificador sea de tamanio 1.
             int dashIndex = rut.LastIndexOf('-');
             if (dashIndex < 0 || rut.Length - dashIndex != 2)
                 return false;
 
+            // Quitar puntos y guión del RUT
             rut = rut.Replace(".", "").Replace("-", "");
 
+            // Verifica que el rut sea de un tamanio correcto.
             if (rut.Length < 9 || rut.Length > 10)
                 return false;
 
+            // Separar el cuerpo del RUT y el dígito verificador
             string dv = rut.Substring(rut.Length - 1, 1);
             string rutBody = rut.Substring(0, rut.Length - 1);
 
+            // Convierte de string rutBody a int rutNumber.
             if (!int.TryParse(rutBody, out int rutNumber))
                 return false;
 
+            //Compara el digito verificador con el resultado del calculo de este mismo a partir del cuerpo del rut. 
             return dv.ToUpper() == CalculateRutCheckDigit(rutNumber);
         }
 
+        /// <summary>
+        /// Calcula el dígito verificador de un RUT chileno basado en el algoritmo de validación.
+        /// </summary>
+        /// <param name="rut">Parametro de tipo int que representa el Número del RUT sin el dígito verificador.</param>
+        /// <returns>Devuelve el dígito verificador como string ("0"-"9" o "K").</returns>
         public static string CalculateRutCheckDigit(int rut)
         {
             int sum = 0;
