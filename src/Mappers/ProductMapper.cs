dd0b630f6293f80bdd29334ff00f5f48e10fcfd0
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2024-11-03 14:52:58 -0300

    docs: document mappers.

--- src/Mappers/ProductMapper.cs
+++ src/Mappers/ProductMapper.cs
@@ -3,8 +3,16 @@ using api.src.Models;
 
 namespace api.src.Mappers
 {
+    /// <summary>
+    /// Clase que proporciona métodos de mapeo entre entidades de producto y sus DTOs correspondientes.
+    /// </summary>
     public static class ProductMapper
     {
+        /// <summary>
+        /// Convierte un objeto de tipo Product a su representación DTO.
+        /// </summary>
+        /// <param name="product">Parametro de tipo Product que representa el producto a convertir.</param>
+        /// <returns>Un objeto de tipo ProductDto que representa el producto convertido.</returns>
         public static ProductDto ToProductDto(this Product product)
         {
             return new ProductDto
@@ -17,6 +25,11 @@ namespace api.src.Mappers
             };
         }
 
+        /// <summary>
+        /// Convierte un objeto de tipo CreateProductRequestDto a un objeto de tipo Product.
+        /// </summary>
+        /// <param name="createProductRequestDto">Parametro de tipo CreateProductRequestDto que representa los datos necesarios para crear un nuevo producto.</param>
+        /// <returns>Un objeto de tipo Product que representa el nuevo producto creado.</returns>
         public static Product ToProductFromCreateDto(this CreateProductRequestDto createProductRequestDto)
         {
             return new Product
@@ -28,6 +41,11 @@ namespace api.src.Mappers
             };
         }
 
+        /// <summary>
+        /// Convierte un objeto de tipo UpdateProductRequestDto a un objeto de tipo Product.
+        /// </summary>
+        /// <param name="updateProductRequestDto">Parametro de tipo UpdateProductRequestDto que representa los datos necesarios para actualizar un producto existente.</param>
+        /// <returns>Un objeto de tipo Product que representa el producto actualizado.</returns>
         public static Product ToProductFromUpdateDto(this UpdateProductRequestDto updateProductRequestDto)
         {
             return new Product
--- src/Mappers/PurchaseMapper.cs
+++ src/Mappers/PurchaseMapper.cs
@@ -3,8 +3,16 @@ using api.src.Models;
 
 namespace api.src.Mappers
 {
+    /// <summary>
+    /// Clase que proporciona métodos de mapeo entre entidades de compra y sus DTOs correspondientes.
+    /// </summary>
     public static class PurchaseMapper
     {
+        /// <summary>
+        /// Convierte un objeto de tipo CreatePurchaseDto a un objeto de tipo Purchase.
+        /// </summary>
+        /// <param name="purchaseDto">Parametro de tipo CreatePurchaseDto que representa los datos necesarios para crear una nueva compra.</param>
+        /// <returns>Un objeto de tipo Purchase que representa la nueva compra creada.</returns>
         public static Purchase ToPurchaseFromCreateDto(this CreatePurchaseDto purchaseDto)
         {
             return new Purchase
@@ -16,6 +24,12 @@ namespace api.src.Mappers
             };
         }
 
+        /// <summary>
+        /// Convierte un objeto de tipo Purchase a su representación DTO.
+        /// </summary>
+        /// <param name="purchase">Parametro de tipo Purchase que representa la compra a convertir.</param>
+        /// <param name="saleItemDtos">Parametro de tipo List<SaleItemDto> que representa la lista de elementos de venta asociados a la compra.</param>
+        /// <returns>Un objeto de tipo PurchaseDto que representa la compra convertida.</returns>
         public static PurchaseDto ToPurchaseDto(this Purchase purchase, List<SaleItemDto> saleItemDtos)
         {
             return new PurchaseDto
@@ -30,12 +44,21 @@ namespace api.src.Mappers
             };
         }
 
+        /// <summary>
+        /// Convierte una lista de objetos SaleItem a una lista de SaleItemDto.
+        /// </summary>
+        /// <param name="saleItems">Parametro de tipo List<SaleItem> que representa la lista de elementos de venta a convertir.</param>
+        /// <param name="products">Parametro de tipo List<Product> que representa la lista de productos asociados a los elementos de venta.</param>
+        /// <returns>Una lista de objetos de tipo SaleItemDto que representan los elementos de venta convertidos.</returns>
+        /// <exception cref="ArgumentNullException">Se lanza si no se encuentra un producto asociado al elemento de venta.</exception>
         public static List<SaleItemDto> ToSaleItemDto(List<SaleItem> saleItems, List<Product> products)
         {
+            // Crea una lista de elementos de tipo SaleItemDto.
             var saleItemDtos = new List<SaleItemDto>();
 
             foreach (var saleItem in saleItems)
             {
+                // Busca el producto asociado al saleItem.
                 var product = products.FirstOrDefault(x => x.Id == saleItem.ProductId);
 
                 if (product == null)
@@ -43,6 +66,7 @@ namespace api.src.Mappers
                     throw new ArgumentNullException(nameof(product));
                 }
 
+                // Aniade un saleItemDto a la lista.
                 saleItemDtos.Add(new SaleItemDto
                 {
                     ProductName = product.Name,
